c29169f206cb8c241be8bc2428a33d77fa8020a1
[nati] <ertacspotify@gmail.com>
2025-10-22 00:42:56 +0200

    [nati] Authorize Supabase image domain - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "xiioavnjoipzglocvjec.supabase.co",
+        port: "",
+        pathname: "/**",
+      }
     ],
   },
   webpack: (config) => {
