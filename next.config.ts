9bef71877107cacf77b44f48eb625d7db96cb863
[nati] <ertacspotify@gmail.com>
2025-10-21 23:26:25 +0200

    [nati] Allowing Unsplash images in Next.js config - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   webpack: (config) => {
     if (process.env.NODE_ENV === "development") {
       config.module.rules.push({
